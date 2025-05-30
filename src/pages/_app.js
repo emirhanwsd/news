47d89f3775d59918aa961e63569e01fcede3f475
emirhanwsd <leetpocketmine@gmail.com>
2025-05-30 21:01:18 +0300

    Remove log

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,8 @@
 import "@/styles/globals.css";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import dayjs from "dayjs";
-import { API_URL } from "@/utils/helper";
 
 dayjs.extend(localizedFormat);
-
-console.log(API_URL);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
