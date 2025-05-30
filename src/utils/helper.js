2199044d491a20354502b66f3a6bf3c02a2598ae
emirhanwsd <leetpocketmine@gmail.com>
2025-05-30 20:57:29 +0300

    Change API URL

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,4 +1,4 @@
-const API_URL = `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`}/api`;
+const API_URL = `${process.env.NODE_ENV === "development" ? "http://localhost:3000" : `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`}/api`;
 
 const leagues = ["eng.1", "esp.1", "ger.1", "ita.1", "fra.1", "por.1", "tur.1"];
 
