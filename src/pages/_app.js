ac4fceee1c81a94cf3da19cc03b6419fd15da33f
emirhanwsd <leetpocketmine@gmail.com>
2025-05-30 20:47:18 +0300

    Log API URL

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,11 @@
 import "@/styles/globals.css";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import dayjs from "dayjs";
+import { API_URL } from "@/utils/helper";
 
 dayjs.extend(localizedFormat);
+
+console.log(API_URL);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
